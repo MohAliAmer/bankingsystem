8d86d63b89739a632a976d732aa1c210a0275f4f
Khaled Moussa <khaled.gnu@gmail.com>
2020-11-10 12:41:08 +0100

    Starting to implement the User interface class

---
+++ src/userinterface.cpp
@@ -0,0 +1,12 @@
+/*
+ * userinterface.cpp
+ *
+ *  Created on: Nov 10, 2020
+ *      Author: amira
+ */
+
+
+#include "userinterface.h"
+
+
+
---
+++ src/userinterface.h
@@ -0,0 +1,30 @@
+/*
+ * userinterface.h
+ *
+ *  Created on: Nov 10, 2020
+ *      Author: amira
+ */
+
+#ifndef SRC_USERINTERFACE_H_
+#define SRC_USERINTERFACE_H_
+
+#include <string>
+#include "session.h"
+#include "person.h"
+#include "customer.h"
+#include "employee.h"
+#include "admin.h"
+
+
+
+class Ui {
+	
+private:
+	
+public:
+	
+};
+
+
+
+#endif /* SRC_USERINTERFACE_H_ */
